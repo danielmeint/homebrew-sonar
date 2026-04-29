e88d560788c317bfb973bc29a65742eb745efd08
Daniel Meint <danimeint@gmail.com>
2026-04-29 15:10:07 +0200

    Downgrade to 0.9.0.977 for CI test

--- Formula/sonar.rb
+++ Formula/sonar.rb
@@ -1,25 +1,25 @@
 class Sonar < Formula
   desc "CLI tool for SonarQube"
   homepage "https://github.com/SonarSource/sonarqube-cli"
-  version "0.10.0.1266"
+  version "0.9.0.977"
   license "LGPL-3.0-only"
 
   on_macos do
     on_arm do
       url "https://binaries.sonarsource.com/Distribution/sonarqube-cli/#{version}/macos/sonarqube-cli-#{version}-macos-arm64.exe"
-      sha256 "25920f532c49fe407fe006c8f6e5990d4a8fe640e9d3b5c200e6df3f1295b68e"
+      sha256 "fakehash_macos"
     end
   end
 
   on_linux do
     on_intel do
       url "https://binaries.sonarsource.com/Distribution/sonarqube-cli/#{version}/linux/sonarqube-cli-#{version}-linux-x86-64.exe"
-      sha256 "a391cb0798dcfe364d6af86107586ec6d5d735a76ebbe8a056c9d33e871bb7d1"
+      sha256 "fakehash_linux_x64"
     end
 
     on_arm do
       url "https://binaries.sonarsource.com/Distribution/sonarqube-cli/#{version}/linux/sonarqube-cli-#{version}-linux-arm64.exe"
-      sha256 "234eb17a7a464f073bfadf12fb6e552e34fbfb2051694db088656331b34a780d"
+      sha256 "fakehash_linux_arm64"
     end
   end
 
